4467bb8df0e3e28eed166e3c555acd4c78590ec6
cunhasb CUNHA <cunhasb@aol.com>
2017-09-27 17:03:55 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,12 @@ require 'yaml'
 
 def load_library(path)
   library = YAML.load_file(path)
+  library.each.with_object do |(meaning,emoticon),new_hash|
+    new_hash["get_meaning"] ||= {}
+    new_hash["get_emoticon"] || {}
+    new_hash["get_meaning"][meaning]=emoticon[1]
+    new_hash["get_emoticon"][meaning] emoticon[0]
+  end
  binding.pry
   # code goes here
 end
