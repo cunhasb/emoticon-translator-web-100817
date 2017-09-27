10554d98d187f801ab070632742c6b12e49b138e
cunhasb CUNHA <cunhasb@aol.com>
2017-09-27 17:12:53 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,10 @@ def load_library(path)
    x = library.each_with_object({}) do |(meaning,emoticon),new_hash|
     new_hash["get_meaning"] ||= {}
     new_hash["get_emoticon"] || {}
+    binding.pry
     new_hash["get_meaning"][meaning] = emoticon[1]
     new_hash["get_emoticon"][meaning] = emoticon[0]
   end
-  binding.pry
   # code goes here
 end
 
